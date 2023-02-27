1ad6ac6ddca4836736fe680a1cd18be8b32ac3ef
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2023-02-27 21:15:55 +0200

    green-test: read json file from assets and parse it

--- app/src/main/kotlin/dev/shaarawy/githubtrends/data/repos/HexColorRepo.kt
+++ app/src/main/kotlin/dev/shaarawy/githubtrends/data/repos/HexColorRepo.kt
@@ -2,6 +2,11 @@ package dev.shaarawy.githubtrends.data.repos
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.shaarawy.githubtrends.foundation.json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.jsonPrimitive
 import javax.inject.Inject
 
 interface HexColorRepo {
@@ -12,6 +17,10 @@ class HexColorRepoImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : HexColorRepo {
     override suspend fun provideForLanguage(language: String): String? {
-        TODO("not implemented")
+        val jsonString =
+            context.assets.open("GithubLanguages.json").bufferedReader().use { it.readText() }
+        return json.decodeFromString<JsonElement>(jsonString)
+            .jsonObject[language]
+            ?.jsonObject?.get("color")?.jsonPrimitive?.content
     }
 }
