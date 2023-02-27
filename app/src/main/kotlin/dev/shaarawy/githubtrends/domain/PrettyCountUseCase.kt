35933cc87f7596bbf48ee220f34b02e0d036e2ff
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2023-02-27 11:19:09 +0200

    green-test: add test code to PrettyCountUseCaseImpl that make PrettyCountUseCaseTests pass

--- app/src/main/kotlin/dev/shaarawy/githubtrends/domain/PrettyCountUseCase.kt
+++ app/src/main/kotlin/dev/shaarawy/githubtrends/domain/PrettyCountUseCase.kt
@@ -1,13 +1,27 @@
 package dev.shaarawy.githubtrends.domain
 
 
+import dev.shaarawy.githubtrends.domain.dtos.Multiplier
 import dev.shaarawy.githubtrends.domain.dtos.PrettyCount
+import java.math.BigDecimal
+import java.math.RoundingMode
 import javax.inject.Inject
 
 interface PrettyCountUseCase : (Int) -> PrettyCount
 class PrettyCountUseCaseImpl @Inject constructor() : PrettyCountUseCase {
     override fun invoke(number: Int): PrettyCount {
-        TODO("no implementation")
-    }
+        val millions = BigDecimal(number / 1_000_000.00)
+        val prettyMillion = millions.setScale(1, RoundingMode.HALF_UP)
+        if (millions >= BigDecimal(1)) {
+            return PrettyCount(prettyMillion.toDouble(), Multiplier.M)
+        }
 
+        val thousands = BigDecimal(number / 1_000.00)
+        val prettyThousands = thousands.setScale(1, RoundingMode.HALF_UP)
+        if (thousands >= BigDecimal(1)) {
+            return PrettyCount(prettyThousands.toDouble(), Multiplier.K)
+        }
+
+        return PrettyCount(number.toDouble(), Multiplier.Non)
+    }
 }
