6da5e47dc43a1272525dc7f2ca3f8dfbcae71a62
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2023-02-27 10:25:04 +0200

    green-test: make all tests in TrendingRepoUseCaseTest succeed

--- app/src/main/kotlin/dev/shaarawy/githubtrends/domain/TrendingRepoUseCase.kt
+++ app/src/main/kotlin/dev/shaarawy/githubtrends/domain/TrendingRepoUseCase.kt
@@ -1,9 +1,12 @@
 package dev.shaarawy.githubtrends.domain
 
 import dev.shaarawy.githubtrends.data.repos.TrendsRepo
+import dev.shaarawy.githubtrends.data.repos.dtos.TrendRepoItem
 import dev.shaarawy.githubtrends.domain.dtos.TrendRepoModel
 import dev.shaarawy.githubtrends.foundation.DispatchersProvider
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface TrendingRepoUseCase : () -> Flow<List<TrendRepoModel>>
@@ -12,5 +15,34 @@ class TrendingRepoUseCaseImpl @Inject constructor(
     private val dispatchersProvider: DispatchersProvider,
     private val trendsRepo: TrendsRepo
 ) : TrendingRepoUseCase {
-    override fun invoke(): Flow<List<TrendRepoModel>> = TODO("missing impl")
+    override fun invoke(): Flow<List<TrendRepoModel>> = trendsRepo.trendsRepoItemsFlow()
+        .map { it.mapNotNull(this::validateAndMapRepoItem) }
+        .flowOn(dispatchersProvider.default)
+
+    private fun validateAndMapRepoItem(input: TrendRepoItem): TrendRepoModel? {
+
+        if (input.id == null || input.name == null ||
+            input.description == null || input.url == null ||
+            input.language == null || input.starsCount == null ||
+            input.owner.id == null || input.owner.name == null ||
+            input.owner.avatarUrl == null || input.owner.url == null
+        ) return null
+
+        return TrendRepoModel(
+            id = input.id,
+            name = input.name,
+            description = input.description,
+            url = input.url,
+            language = input.language,
+            starsCount = input.starsCount,
+            colorHex = "null",
+            owner = TrendRepoModel.Owner(
+                id = input.owner.id,
+                name = input.owner.name,
+                avatarUrl = input.owner.avatarUrl,
+                url = input.owner.url
+            )
+        )
+
+    }
 }
